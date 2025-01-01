c99a02218a7a0904a5dc463bd30229d242bbbb7e
Moiz Ahmed <moizaman905@gmail.com>
2025-01-01 16:54:41 +0500

    updated
    FullStack E-Commerce Web Store

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -9,8 +9,8 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://moiz-full-stack-e-commerce-web.vercel.app/stripe/success"
+      cancelUrl="https://moiz-full-stack-e-commerce-web.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={true}
       shouldPersist={true}
